edc6bb0ae92de6797096e79841bdf7389f61c8c1
benezes <gabriel.menezes3@hotmail.com>
2023-06-08 18:12:17 -0300

    refactor: refactoring main class

--- src/main/java/com/mnz/App.java
+++ src/main/java/com/mnz/App.java
@@ -2,21 +2,18 @@ package com.mnz;
 
 import com.mnz.service.*;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
 
-        if (args.length != 1) {
-            System.out.println("Please provide a file name as an argument.");
-            return;
-        }
-
         INameExtractor extractor = new NameExtractor();
-        List<String> names = extractor.extractNamesToList(args[0]);
+        List<String> names;
+        if (args.length != 1) {
+            names = extractor.extractNamesToList("sorted-names-list.txt");
+        }else {
+            names = extractor.extractNamesToList(args[0]);
+        }
 
         INameSorter sorter = new NameSorter();
         sorter.sortNames(names);
